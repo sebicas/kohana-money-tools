a290edb25d14c79a46f40cc3004741aada2c94a5
Sebastian Castro <sebicas@gmail.com>
2011-04-27 19:45:37 -0300

    Model_Money_Exchage_Rate() created... works with MongoDB

---
+++ classes/model/money/exchange/rate.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Model_Money_Exchange_Rate extends Mongo_Document {
+
+    // Collection Name
+    protected $name = 'exchange_rates';
+
+}
